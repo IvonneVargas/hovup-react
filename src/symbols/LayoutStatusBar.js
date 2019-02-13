862f7d8bde055f0a06faa16b2275ee4194714391
Ivonne Vargas <ivonnegvargas@gmail.com>
2019-02-13 13:04:26 -0600

    add new symbol for status bar and new screen list memberships for tabs:hatching_chick:

---
+++ src/symbols/LayoutStatusBar.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import { View, StyleSheet } from "react-native";
+
+export default class LayoutStatusBar extends Component {
+  // Only for displaying symbol in BuilderX.
+  static containerStyle = {
+    width: 376,
+    height: 3
+  };
+  render() {
+    return <View style={[styles.root, this.props.style]} />;
+  }
+}
+const styles = StyleSheet.create({
+  root: {
+    backgroundColor: "rgba(48,61,73,1)",
+    opacity: 1
+  }
+});
