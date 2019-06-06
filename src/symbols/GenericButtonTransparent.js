7e8165febc66b40252f1caefa38f7fd7cdd9f55d
Ivonne Vargas <ivonnegvargas@gmail.com>
2019-06-06 15:23:34 -0500

    add new bottom transparent

---
+++ src/symbols/GenericButtonTransparent.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+
+import { View, TouchableOpacity, StyleSheet, Text } from "react-native";
+
+export default class GenericButton extends Component {
+  // Only for displaying symbol in BuilderX.
+  static containerStyle = {
+    width: 264,
+    height: 42
+  };
+  render() {
+    return (
+      <TouchableOpacity
+        style={[styles.root, this.props.style]}
+        onPress={
+          this.props.root ? (
+            this.props.root
+          ) : (
+            () => {
+              this.props.navigation.push("Launcher");
+            }
+          )
+        }
+      >
+        <Text style={styles.text}>
+          {this.props.text ? this.props.text : "Text Added"}
+        </Text>
+      </TouchableOpacity>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  text: {
+    top: 15,
+    left: "0%",
+    width: 264,
+    height: 16,
+    position: "absolute",
+    backgroundColor: "transparent",
+    textAlign: "center",
+    color: "rgba(126,211,33,1)"
+  },
+
+  root: {
+    borderRadius: 7
+  }
+});
