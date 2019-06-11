ae1619d459ce83adddb2a4b52059004e5a1b9828
Ivonne Vargas <ivonnegvargas@gmail.com>
2019-06-11 18:22:41 -0500

    init screen for buy

--- src/screens/Wallet.js
+++ src/screens/Wallet.js
@@ -2,18 +2,20 @@ import React, { Component } from "react";
 import LayoutStatusBar from "../symbols/LayoutStatusBar";
 import HeaderSingleLogo from "../symbols/HeaderSingleLogo";
 
-import HeaderBack from "../symbols/HeaderBack";
 import TabViewButton from "../symbols/TabViewButton";
 import Icon from "@builderx/icons";
+import HeaderBack from "../symbols/HeaderBack";
+import Title from "../symbols/Title";
+import Subtitle from "../symbols/Subtitle";
 import {
   View,
   StyleSheet,
   StatusBar,
   TouchableOpacity,
   Image,
-  Text,
   FlatList,
-  Platform
+  Platform,
+  Picker
 } from "react-native";
 
 export default class Wallet extends Component {
@@ -45,8 +47,22 @@ export default class Wallet extends Component {
         />
         <View style={styles.background}>
           <LayoutStatusBar style={styles.layoutStatusBar} />
-          {this.displayContent()}
+          <View style={styles.content}>
+            <HeaderBack
+              style={styles.headerBack}
+              navigation={this.props.navigation}
+            />
+            <Title style={styles.title} />
+            <View style={styles.viewProd} />
+          </View>
         </View>
+        <Subtitle style={styles.itemPicker} />
+        <Picker style={styles.icon} itemStyle={{}} enabled={true}>
+          <Picker.Item value="one" label="Lorem Ipsum Dolor Sit" />
+          <Picker.Item value="two" label="Lorem Ipsum Dolor Sit" />
+          <Picker.Item value="three" label="Lorem Ipsum Dolor Sit" />
+        </Picker>
+        <Icon name="home" style={styles.sIes7W} type="MaterialCommunityIcons" />
       </View>
     );
   }
@@ -114,101 +130,101 @@ export default class Wallet extends Component {
         </View>
       );
     } else if (typeT == "coupon") {
-      return(
+      return (
         <View style={styles.viewCoupon}>
-            <HeaderBack
-              style={styles.headerBack}
-              navigation={this.props.navigation}
+          <HeaderBack
+            style={styles.headerBack}
+            navigation={this.props.navigation}
+          />
+          <Text style={styles.text}>Cupones</Text>
+          <TabViewButton
+            style={styles.tabViewButton}
+            text="Bonificacion"
+            text2="Beneficio"
+            text3="Descuento"
+            active={this.state.active}
+            button={() => {
+              this.setState({
+                active: 1,
+                datalist: [
+                  {
+                    key: "Bonificacion",
+                    sub: "Descripcion",
+                    icon: require("../assets/Wallet/ic_ventas.png"),
+                    type: "coupon"
+                  }
+                ]
+              });
+            }}
+            button2={() => {
+              this.setState({
+                active: 2,
+                datalist: [
+                  {
+                    key: "Beneficio",
+                    sub: "Descripcion",
+                    icon: require("../assets/Wallet/ic_ventas.png"),
+                    type: "coupon"
+                  }
+                ]
+              });
+            }}
+            button3={() => {
+              this.setState({
+                active: 3,
+                datalist: [
+                  {
+                    key: "Descuento",
+                    sub: "Descripcion",
+                    icon: require("../assets/Wallet/ic_ventas.png"),
+                    type: "coupon"
+                  }
+                ]
+              });
+            }}
+          />
+          <View style={styles.content}>
+            <Text style={styles.text2}>{this.state.active}</Text>
+            <FlatList
+              style={styles.list}
+              data={this.state.datalist}
+              renderItem={({ item, separators }) => {
+                return (
+                  <View style={styles.rect}>
+                    <TouchableOpacity
+                      style={styles.buttonsStyle}
+                      onPress={() => {
+                        console.log("1Click,", item.level);
+                        this.props.navigation.push("BrandsTab", {
+                          level: item.level,
+                          type: this.props.type
+                        });
+                      }}
+                    >
+                      <Image
+                        source={require("../assets/ic_60x60.png")}
+                        style={styles.imageS}
+                      />
+                      <Icon
+                        name="ios-arrow-forward"
+                        style={styles.icon}
+                        type="Ionicons"
+                      />
+                      <View style={styles.rect9}>
+                        <Text style={styles.text6}>{item.key}</Text>
+                        <Text style={styles.text7}>{item.sub}</Text>
+                      </View>
+                    </TouchableOpacity>
+                  </View>
+                );
+              }}
+              ItemSeparatorComponent={({}) => {
+                return <View style={styles.rect4} />;
+              }}
             />
-            <Text style={styles.text}>Cupones</Text>
-            <TabViewButton
-              style={styles.tabViewButton}
-              text="Bonificacion"
-              text2="Beneficio"
-              text3="Descuento"
-              active={this.state.active}
-              button={() => {
-                this.setState({
-                  active: 1,
-                  datalist: [
-                    {
-                      key: "Bonificacion",
-                      sub: "Descripcion",
-                      icon: require("../assets/Wallet/ic_ventas.png"),
-                      type: "coupon"
-                    }
-                  ]
-                });
-              }}
-              button2={() => {
-                this.setState({
-                  active: 2,
-                  datalist: [
-                    {
-                      key: "Beneficio",
-                      sub: "Descripcion",
-                      icon: require("../assets/Wallet/ic_ventas.png"),
-                      type: "coupon"
-                    }
-                  ]
-                });
-              }}
-              button3={() => {
-                this.setState({
-                  active: 3,
-                  datalist: [
-                    {
-                      key: "Descuento",
-                      sub: "Descripcion",
-                      icon: require("../assets/Wallet/ic_ventas.png"),
-                      type: "coupon"
-                    }
-                  ]
-                });
-              }}
-            />
-            <View style={styles.content}>
-              <Text style={styles.text2}>{this.state.active}</Text>
-              <FlatList
-                style={styles.list}
-                data={this.state.datalist}
-                renderItem={({ item, separators }) => {
-                  return (
-                    <View style={styles.rect}>
-                      <TouchableOpacity
-                        style={styles.buttonsStyle}
-                        onPress={() => {
-                          console.log("1Click,", item.level);
-                          this.props.navigation.push("BrandsTab", {
-                            level: item.level,
-                            type: this.props.type
-                          });
-                        }}
-                      >
-                        <Image
-                          source={require("../assets/ic_60x60.png")}
-                          style={styles.imageS}
-                        />
-                        <Icon
-                          name="ios-arrow-forward"
-                          style={styles.icon}
-                          type="Ionicons"
-                        />
-                        <View style={styles.rect9}>
-                          <Text style={styles.text6}>{item.key}</Text>
-                          <Text style={styles.text7}>{item.sub}</Text>
-                        </View>
-                      </TouchableOpacity>
-                    </View>
-                  );
-                }}
-                ItemSeparatorComponent={({}) => {
-                  return <View style={styles.rect4} />;
-                }}
-              />
-            </View>
           </View>
-        );
+        </View>
+      );
     }
   }
 }
@@ -292,20 +308,10 @@ const styles = StyleSheet.create({
     alignSelf: "stretch"
   },
   headerBack: {
-    height: 54,
+    height: 55,
     alignSelf: "stretch"
   },
-  text: {
-    width: 106.33,
-    height: 31,
-    backgroundColor: "transparent",
-    fontSize: 18,
-    color: "rgba(253,253,253,1)",
-    padding: 0,
-    textAlign: "center",
-    margin: 18,
-    alignSelf: "center"
-  },
+
   tabViewButton: {
     height: 70,
     alignSelf: "stretch"
@@ -370,5 +376,47 @@ const styles = StyleSheet.create({
     height: 17,
     fontSize: 16,
     color: "rgba(255,255,255,1)"
+  },
+  viewProd: {
+    width: 344.24,
+    height: 78,
+    margin: 0,
+    flexDirection: "column",
+    backgroundColor: "rgba(48,61,73,1)",
+    opacity: 1,
+    padding: 0,
+    marginLeft: 0,
+    marginRight: 0,
+    justifyContent: "space-around",
+    alignItems: "center",
+    alignSelf: "center"
+  },
+  title: {
+    height: 23.9,
+    width: 77,
+    alignSelf: "center",
+    margin: 18
+  },
+  itemPicker: {
+    position: "absolute",
+    top: 141,
+    left: 130,
+    height: 100,
+    width: 100
+  },
+  icon: {
+    top: 380.93,
+    left: 163.36,
+    position: "absolute"
+  },
+  sIes7W: {
+    height: 1,
+    width: 1,
+    top: 324.54,
+    left: 63.1,
+    position: "absolute",
+    backgroundColor: "transparent",
+    color: "grey",
+    fontSize: 40
   }
 });
