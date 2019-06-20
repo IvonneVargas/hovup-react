93b33e9b126c6dfc3833402631964ed88f6cf377
Ivonne Vargas <ivonnegvargas@gmail.com>
2019-06-20 18:06:16 -0500

    fix styles

--- src/screens/MembershipTab.js
+++ src/screens/MembershipTab.js
@@ -7,51 +7,51 @@ import {
   View,
   TouchableOpacity,
   Image
-} from 'react-native';
-import { Constants } from 'expo';
-import * as Animatable from 'react-native-animatable';
-import Collapsible from 'react-native-collapsible';
-import Accordion from 'react-native-collapsible/Accordion';
+} from "react-native";
+import { Constants } from "expo";
+import * as Animatable from "react-native-animatable";
+import Collapsible from "react-native-collapsible";
+import Accordion from "react-native-collapsible/Accordion";
 
 const BACON_IPSUM =
-  'Bacon ipsum dolor amet chuck turducken landjaeger tongue spare ribs. Picanha beef prosciutto meatball turkey shoulder shank salami cupim doner jowl pork belly cow. Chicken shankle rump swine tail frankfurter meatloaf ground round flank ham hock tongue shank andouille boudin brisket. ';
+  "Bacon ipsum dolor amet chuck turducken landjaeger tongue spare ribs. Picanha beef prosciutto meatball turkey shoulder shank salami cupim doner jowl pork belly cow. Chicken shankle rump swine tail frankfurter meatloaf ground round flank ham hock tongue shank andouille boudin brisket. ";
 
 const CONTENT = [
   {
-    title: 'First',
+    title: "First",
     content: BACON_IPSUM,
-    color: '#F9060A'
+    color: "#F9060A"
   },
   {
-    title: 'Second',
+    title: "Second",
     content: BACON_IPSUM,
-    color: '#DC045E'
+    color: "#DC045E"
   },
   {
-    title: 'Third',
+    title: "Third",
     content: BACON_IPSUM,
-    color: '#7D09F9'
+    color: "#7D09F9"
   },
   {
-    title: 'Fourth',
+    title: "Fourth",
     content: BACON_IPSUM,
-    color: '#020AF4'
+    color: "#020AF4"
   },
   {
-    title: 'Fifth',
+    title: "Fifth",
     content: BACON_IPSUM,
-    color: '#10FA03'
+    color: "#10FA03"
   },
   {
-    title: 'Six',
+    title: "Six",
     content: BACON_IPSUM,
-    color: '#F4E301'
+    color: "#F4E301"
   },
   {
-    title: 'Seven',
+    title: "Seven",
     content: BACON_IPSUM,
-    color: '#F74302'
-  },
+    color: "#F74302"
+  }
 ];
 
 export default class MembershipTab extends Component {
@@ -71,37 +71,41 @@ export default class MembershipTab extends Component {
 
   setSections = sections => {
     this.setState({
-      activeSections: sections.includes(undefined) ? [] : sections,
+      activeSections: sections.includes(undefined) ? [] : sections
     });
   };
 
   renderHeader = (section, _, isActive) => {
     return (
-      <View style={[styles.header, isActive ? styles.active : styles.inactive]}>
-        <Image
-          style={styles.image}
-          source={require("../assets/ic_200x200.png")}
-        />
-        <View style={styles.label}>
-          <View style={styles.level}>
-            <Text style={styles.levelTextK}>1</Text>
-            <Text style={styles.textLevelL}>LVL</Text>
-          </View>
-          <View style={styles.totalLevel}>
-            <Text style={styles.levelText}>1/10</Text>
+      <View
+          style={[styles.header, isActive ? styles.active : styles.inactive]}
+        >
+          <Image
+            style={styles.image}
+            source={require("../assets/ic_200x200.png")}
+          />
+          <View style={styles.label}>
+            <View style={styles.level}>
+              <Text style={styles.levelTextK}>1</Text>
+              <Text style={styles.textLevelL}>LVL</Text>
+            </View>
+            <View style={styles.totalLevel}>
+              <Text style={styles.levelText}>1/10</Text>
+            </View>
           </View>
         </View>
-      </View>
     );
   };
 
   renderContent(section, _, isActive) {
     return (
-      <View style={[styles.content, isActive ? styles.active : styles.inactive]}>
+      <View
+        style={[styles.content, isActive ? styles.active : styles.inactive]}
+      >
         <Text>{section.content}</Text>
       </View>
     );
-  };
+  }
 
   render() {
     const { multipleSelect, activeSections } = this.state;
@@ -116,59 +120,62 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "rgba(25,39,52,1)",
-    paddingTop: Constants.statusBarHeight,
+    paddingTop: Constants.statusBarHeight
   },
   title: {
-    textAlign: 'center',
+    textAlign: "center",
     fontSize: 22,
-    fontWeight: '300',
-    marginBottom: 20,
+    fontWeight: "300",
+    marginBottom: 20
   },
   header: {
     backgroundColor: "rgba(48,61,73,1)",
-    padding: 10
+    padding: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    height: 84
   },
   headerText: {
-    textAlign: 'center',
+    textAlign: "center",
     fontSize: 16,
-    fontWeight: '500',
+    fontWeight: "500"
   },
   content: {
     padding: 20,
-    backgroundColor: "rgba(25,39,52,1)",
+    backgroundColor: "rgba(25,39,52,1)"
   },
   active: {
-    backgroundColor: "rgba(48,61,73,1)",
+    backgroundColor: "rgba(48,61,73,1)"
   },
   inactive: {
-    backgroundColor: "rgba(25,39,52,1)",
+    backgroundColor: "rgba(25,39,52,1)"
   },
   selectors: {
     marginBottom: 10,
-    flexDirection: 'row',
-    justifyContent: 'center',
+    flexDirection: "row",
+    justifyContent: "center"
   },
   selector: {
     backgroundColor: "rgba(25,39,52,1)",
-    padding: 10,
+    padding: 10
   },
   activeSelector: {
-    fontWeight: 'bold',
+    fontWeight: "bold"
   },
   selectTitle: {
     fontSize: 14,
-    fontWeight: '500',
-    padding: 10,
+    fontWeight: "500",
+    padding: 10
   },
   multipleToggle: {
-    flexDirection: 'row',
-    justifyContent: 'center',
+    flexDirection: "row",
+    justifyContent: "center",
     marginVertical: 30,
-    alignItems: 'center',
+    alignItems: "center"
   },
   multipleToggle__title: {
     fontSize: 16,
-    marginRight: 8,
+    marginRight: 8
   },
   image: {
     width: 95,
@@ -206,7 +213,7 @@ const styles = StyleSheet.create({
     height: 38.93,
 
     backgroundColor: "transparent",
-    fontSize: 40,
+    fontSize: 30,
     color: "rgba(249,249,249,1)",
     textAlign: "center",
     alignSelf: "center"
