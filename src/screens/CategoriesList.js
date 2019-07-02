88c5b2aaf618e845b3e33bc07742c2ca2aff4de3
Ivonne Vargas <ivonnegvargas@gmail.com>
2019-07-02 18:10:41 -0500

    add wrapper in categories

--- src/screens/CategoriesList.js
+++ src/screens/CategoriesList.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-import Header from "../symbols/Header";
 import LayoutStatusBar from "../symbols/LayoutStatusBar";
-import { View, StyleSheet, FlatList, Text, Platform } from "react-native";
+import Header from "../symbols/Header";
+
+import { View, StyleSheet, Platform, FlatList, Text } from "react-native";
 
 export default class CategoriesList extends Component {
   render() {
@@ -14,56 +15,27 @@ export default class CategoriesList extends Component {
     }
     return (
       <View style={styles.root}>
-        <Header
-          style={styles.header}
-          navigation={this.props.navigation}
-          text={title}
-        />
-        <LayoutStatusBar style={styles.layoutStatusBar} />
-        <FlatList
-          style={styles.list}
-          data={[
-            {
-              key: "Perfil",
-              link: ""
-            },
-            {
-              icon: require("../assets/OptionsImages/ic_perfil.png"),
-              key: "Cambiar codigo Hovup",
-              link: ""
-            },
-            {
-              icon: require("../assets/OptionsImages/ic_perfil.png"),
-              key: "Cambiar contrasena",
-              link: ""
-            },
-            {
-              icon: require("../assets/OptionsImages/ic_perfil.png"),
-              key: "Acerca de nosotros",
-              link: ""
-            },
-            {
-              icon: require("../assets/OptionsImages/ic_perfil.png"),
-              key: "Aviso de privacidad",
-              link: ""
-            },
-            {
-              icon: require("../assets/OptionsImages/ic_perfil.png"),
-              key: "Cerrar sesion",
-              link: ""
-            }
-          ]}
-          renderItem={({ item, separators }) => {
-            return (
-              <View style={styles.rect}>
-                <Text style={styles.text}>List Item</Text>
-              </View>
-            );
-          }}
-          ItemSeparatorComponent={({}) => {
-            return <View style={styles.rect2} />;
-          }}
-        />
+        <View style={styles.backgroundView}>
+          <LayoutStatusBar style={styles.layoutStatusBar} />
+          <Header
+            style={styles.header}
+            navigation={this.props.navigation}
+            text="title"
+          />
+          <FlatList
+            style={styles.list}
+            renderItem={({ item, separators }) => {
+              return (
+                <View style={styles.rect}>
+                  <Text style={styles.text}>List Item</Text>
+                </View>
+              );
+            }}
+            ItemSeparatorComponent={({}) => {
+              return <View style={styles.rect2} />;
+            }}
+          />
+        </View>
       </View>
     );
   }
@@ -73,43 +45,46 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     flex: 1
   },
-  header: {
-    top: Platform.OS === "android" ? 24 : 28,
-    left: 0,
-    position: "absolute",
-    height: 53,
-    right: 0
-  },
-  layoutStatusBar: {
+  backgroundView: {
     top: 0,
     left: 0,
     position: "absolute",
-    height: Platform.OS === "android" ? 25 : 32,
-    right: 0
+    right: 0,
+    bottom: 0,
+    flexDirection: "column"
+  },
+  layoutStatusBar: {
+    height: Platform.OS === "android" ? 25 : 33,
+    alignSelf: "stretch"
+  },
+  header: {
+    top: Platform.OS === "android" ? 24 : undefined,
+    height: 53,
+    alignSelf: "stretch"
   },
   list: {
-    top: Platform.OS === "android" ? 75.67 : 81,
-    left: Platform.OS === "android" ? 0 : 0,
-    position: "absolute",
-    right: Platform.OS === "android" ? 0 : 0,
-    bottom: Platform.OS === "android" ? undefined : -81,
+    top: Platform.OS === "android" ? 75.67 : undefined,
+    bottom: Platform.OS === "android" ? "" : undefined,
+    right: Platform.OS === "android" ? 0 : undefined,
+    height: Platform.OS === "android" ? 745 : undefined,
     backgroundColor: "rgba(25,39,52,1)",
     opacity: 1,
-    height: Platform.OS === "android" ? 745 : undefined
+    alignSelf: "stretch",
+    flex: 1
   },
   rect: {
     backgroundColor: "rgba(25,39,52,1)",
+    opacity: 1,
     padding: 15,
     paddingTop: 10,
-    paddingBottom: 10,
-    opacity: 1
+    paddingBottom: 10
   },
   text: {
     color: "rgba(255,255,255,1)"
   },
   rect2: {
-    backgroundColor: "#999999",
     left: 15,
-    height: 2
+    height: 2,
+    backgroundColor: "#999999"
   }
 });
