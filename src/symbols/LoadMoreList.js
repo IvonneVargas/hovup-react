dbbe9571a6488cc8e0ab2a9cc47543d1ac7673a4
Ivonne Vargas <ivonnegvargas@gmail.com>
2019-07-02 12:53:46 -0500

    fix lines

--- src/symbols/LoadMoreList.js
+++ src/symbols/LoadMoreList.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
   View,
   Text,
@@ -6,41 +6,37 @@ import {
   StyleSheet,
   TouchableOpacity,
   AppRegistry
-} from 'react-native';
+} from "react-native";
 
-import { ListView, GotoTopCreator } from 'react-native-refreshable-useful';
+import { ListView, GotoTopCreator } from "react-native-refreshable-useful";
 
-const ListViewWithScrollController = GotoTopCreator ( ListView );
+const ListViewWithScrollController = GotoTopCreator(ListView);
 
 export default class LoadMoreList extends Component {
-  constructor ( props ) {
-    super ( props );
+  constructor(props) {
+    super(props);
 
     this.pageSize = 10;
   }
 
   componentDidMount() {
-
-    this.refs.listView.onRefresh ( );
+    this.refs.listView.onRefresh();
   }
 
-  onRefresh ( fn, params ) {
-
-    this.getItems ( fn, 1 )
+  onRefresh(fn, params) {
+    this.getItems(fn, 1);
   }
 
-  getItems ( fn, page ) {
-
+  getItems(fn, page) {
     let list = [];
 
-    let currLayer = ( page - 1 ) * this.pageSize + 1;
+    let currLayer = (page - 1) * this.pageSize + 1;
 
     let loadLayer = currLayer + this.pageSize;
 
-    if ( loadLayer > 100 ) loadLayer -= 1; // hasMore = false
-    
-    for (; currLayer < loadLayer; currLayer++) {
+    if (loadLayer > 100) loadLayer -= 1; // hasMore = false
 
+    for (; currLayer < loadLayer; currLayer++) {
       list.push({
         key: "1",
         date: "01/09/2018",
@@ -51,47 +47,46 @@ export default class LoadMoreList extends Component {
       });
     }
 
-    fn && 
-    setTimeout(()=>{
-
-      fn ( list )
-    }, 500);
+    fn &&
+      setTimeout(() => {
+        fn(list);
+      }, 500);
 
     return list;
   }
 
-  onLoadMore ( fn, page ) {
-
-    this.getItems ( fn, page )
+  onLoadMore(fn, page) {
+    this.getItems(fn, page);
   }
 
-  renderRow ( { item, index } ) {
-
-    return (<View style={index % 2 != 0 ? (
-                            [
-                              S.row,
-                              { backgroundColor: this.props.section.color + "1A" }
-                            ]
-                          ) : (
-                            S.row
-                          )}>
-                <View style={S.cellOne}>
-                  <Text style={S.textDataData}>{item.date}</Text>
-                </View>
-                <View style={S.cellTwo}>
-                  <Text style={S.textEvent}>{item.events}</Text>
-                </View>
-                <View style={S.cellThree}>
-                  <Text style={S.textPointsData}>{item.points}</Text>
-                </View>
-                <View style={S.cellFour}>
-                  <Text style={S.textView}>{item.days}</Text>
-                </View>
-              </View>);
+  renderRow({ item, index }) {
+    return (
+      <View
+        style={
+          index % 2 != 0 ? (
+            [S.row, { backgroundColor: this.props.section.color + "1A" }]
+          ) : (
+            S.row
+          )
+        }
+      >
+        <View style={S.cellOne}>
+          <Text style={S.textDataData}>{item.date}</Text>
+        </View>
+        <View style={S.cellTwo}>
+          <Text style={S.textEvent}>{item.events}</Text>
+        </View>
+        <View style={S.cellThree}>
+          <Text style={S.textPointsData}>{item.points}</Text>
+        </View>
+        <View style={S.cellFour}>
+          <Text style={S.textView}>{item.days}</Text>
+        </View>
+      </View>
+    );
   }
 
-  renderGotoTop ( ) {
-
+  renderGotoTop() {
     let props = {
       props: {
         hideStyle: { bottom: -100 },
@@ -103,8 +98,7 @@ export default class LoadMoreList extends Component {
     return props;
   }
 
-  renderFooter ( ) {
-
+  renderFooter() {
     let props = {
       style: {
         height: 44
@@ -119,85 +113,89 @@ export default class LoadMoreList extends Component {
     return props;
   }
 
-  onScroll(evt) {
+  onScroll(evt) {}
 
-  }
-
-  onEndReached ( ) {
-
+  onEndReached() {
     /* just onEndReached */
   }
 
-  ItemSeparatorComponent(){
-            return <View style={S.rect4} />;
+  ItemSeparatorComponent() {
+    return <View style={S.rect4} />;
   }
 
   render() {
-
     return (
       <View style={S.container}>
         <ListViewWithScrollController
           ref="listView"
-          debug={false}                                    /* (optional) debug mode [ test only ] */
-          data={ this.getItems ( null, 1 ) }              /* (optional) init list */
-          pageSize={ 10 }
-          dftFooter                                       /* (optional) use default loadMore component */
-          onScroll={this.onScroll.bind(this)}             /* (optional) */
-          onRefresh={this.onRefresh.bind(this)}           /* (optional) refresh data */
-          ItemSeparatorComponent={ this.ItemSeparatorComponent }        /* (optional) ... */
-          renderRow={this.renderRow.bind(this)}           /* (required) ... */
-          renderFooter={this.renderFooter.bind(this)}     /* (optional) footer component / loadMore options */
-          renderGotoTop={this.renderGotoTop.bind(this)}   /* (optional) scrollTo(y:0) controller */
-          onLoadMore={ this.onLoadMore.bind ( this ) }    /* (optional) onEndReached & hasMore */
+          debug={false} /* (optional) debug mode [ test only ] */
+          data={this.getItems(null, 1)} /* (optional) init list */
+          pageSize={10}
+          dftFooter /* (optional) use default loadMore component */
+          onScroll={this.onScroll.bind(this)} /* (optional) */
+          onRefresh={this.onRefresh.bind(this)} /* (optional) refresh data */
+          ItemSeparatorComponent={
+            this.ItemSeparatorComponent
+          } /* (optional) ... */
+          renderRow={this.renderRow.bind(this)} /* (required) ... */
+          renderFooter={this.renderFooter.bind(
+            this
+          )} /* (optional) footer component / loadMore options */
+          renderGotoTop={this.renderGotoTop.bind(
+            this
+          )} /* (optional) scrollTo(y:0) controller */
+          onLoadMore={this.onLoadMore.bind(
+            this
+          )} /* (optional) onEndReached & hasMore */
           /* ...any FlatList props */
           /* more info and option: please view source :) */
         />
       </View>
-    )
+    );
   }
 }
 const S = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#EEE'
+    backgroundColor: "#EEE"
   },
   titleBar: {
     height: 64,
     paddingTop: 20,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#333',
-    borderBottomColor: '#000',
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#333",
+    borderBottomColor: "#000",
     borderBottomWidth: StyleSheet.hairlineWidth
   },
   titleText: {
-    color: '#EEE'
+    color: "#EEE"
   },
   separator: {
     height: StyleSheet.hairlineWidth,
-    backgroundColor: '#DDD'
+    backgroundColor: "#DDD"
   },
   row: {
-    alignItems: 'center',
-    backgroundColor: 'rgba(255, 255, 255, 0.2)',
-    flexDirection: 'row',
+    alignItems: "center",
+    backgroundColor: "rgba(255, 255, 255, 0.2)",
+    flexDirection: "row",
     padding: 6
   },
   rowChecked: {
-    backgroundColor: '#666'
+    backgroundColor: "#666"
   },
   rowCheckedTitle: {
-    color: '#EEE'
+    color: "#EEE"
   },
   rowDesc: {
     marginTop: 12,
     fontSize: 12,
-    color: '#888'
+    color: "#888"
   },
   cloudIcon: {
     width: 40,
     height: 40,
-    resizeMode: 'contain'
+    resizeMode: "contain"
   },
   rect4: {
     backgroundColor: "#999999",
