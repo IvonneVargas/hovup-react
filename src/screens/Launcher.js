9f4427ad5aa232af0f2f8261b54b2586d790fc35
Ivonne Vargas <ivonnegvargas@gmail.com>
2019-07-03 12:28:39 -0500

    add container! in login main

--- src/screens/Launcher.js
+++ src/screens/Launcher.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
-import LogoR from "../symbols/LogoR";
+
 import { Center } from "@builderx/utils";
 
+import LayoutStatusBar from "../symbols/LayoutStatusBar";
+import LogoR from "../symbols/LogoR";
 import GenericButton from "../symbols/GenericButton";
+import Others from "../symbols/Others";
 import GenericButtonIcon from "../symbols/GenericButtonIcon";
 import GenericButtonImage from "../symbols/GenericButtonImage";
-import Others from "../symbols/Others";
+
 import {
   View,
   StyleSheet,
@@ -13,8 +16,8 @@ import {
   ScrollView,
   Alert,
   Button,
-  Text,
-  Platform
+  Platform,
+  Text
 } from "react-native";
 
 export default class Launcher extends Component {
@@ -22,44 +25,45 @@ export default class Launcher extends Component {
     return (
       <View style={styles.root}>
         <StatusBar barStyle="light-content" style={styles.statusBar} />
-        <ScrollView style={styles.scrollArea} />
-        <Center horizontal>
-          <LogoR style={styles.logoR} />
-        </Center>
-        <View style={styles.containerButtons}>
-          <Text style={styles.text2}>Inicia sesion o crea una cuenta</Text>
-          <GenericButton
-            style={styles.genericButton4}
-            navigation={this.props.navigation}
-            text="Iniciar con correo electronico"
-            root={() => {
-              this.props.navigation.push("Login");
-            }}
-          />
-          <GenericButton
-            style={styles.genericButton2}
-            navigation={this.props.navigation}
-            text="Crear cuenta"
-            root={() => {
-              this.props.navigation.push("CreateAccount");
-            }}
-          />
-          <Others style={styles.others} />
-          <GenericButtonIcon
-            style={styles.genericButtonIcon}
-            text="Inicia con Facebook"
-          />
-          <GenericButtonImage
-            style={styles.genericButtonImage}
-            text="Iniciar con Google"
-          />
-          <GenericButton
-            style={styles.genericButton3}
-            navigation={this.props.navigation}
-            text="Iniciar como invitado"
-          />
+        <View style={styles.background}>
+          <LayoutStatusBar style={styles.layoutStatusBar} />
+          <View style={styles.rect} navigation={this.props.navigation}>
+            <LogoR style={styles.logoR} />
+            <View style={styles.containerButtons}>
+              <Text style={styles.text2}>Inicia sesion o crea una cuenta</Text>
+              <GenericButton
+                style={styles.genericButton4}
+                navigation={this.props.navigation}
+                text="Iniciar con correo electronico"
+                root={() => {
+                  this.props.navigation.push("Login");
+                }}
+              />
+              <GenericButton
+                style={styles.genericButton2}
+                navigation={this.props.navigation}
+                text="Crear cuenta"
+                root={() => {
+                  this.props.navigation.push("CreateAccount");
+                }}
+              />
+              <Others style={styles.others} />
+              <GenericButtonIcon
+                style={styles.genericButtonIcon}
+                text="Inicia con Facebook"
+              />
+              <GenericButtonImage
+                style={styles.genericButtonImage}
+                text="Iniciar con Google"
+              />
+              <GenericButton
+                style={styles.genericButton3}
+                navigation={this.props.navigation}
+                text="Iniciar como invitado"
+              />
+            </View>
+          </View>
         </View>
-        <View style={styles.rect} />
       </View>
     );
   }
@@ -71,45 +75,11 @@ const styles = StyleSheet.create({
   },
   statusBar: {},
 
-  scrollArea: {
-    top: 0,
-    left: 0,
-    bottom: 0,
-    right: 0,
-    position: "absolute",
-    backgroundColor: "#192734",
-    opacity: 1
-  },
-  logoR: {
-    top: 106.85,
-    position: "absolute",
-    height: 164,
-    width: 265
-  },
-  containerButtons: {
-    height: 337,
-    top: 271,
-    left: 0,
-    position: "absolute",
-
-    right: 0,
-    alignItems: "center",
-    justifyContent: "space-between"
-  },
-
   button1: {
     top: 598.27,
 
     position: "absolute",
     height: 44
-  },
-
-  text2: {
-    width: 262,
-    height: 17,
-    backgroundColor: "transparent",
-    textAlign: "center",
-    color: "#65bc46"
   },
 
   buttonGreen2: {
@@ -136,36 +106,45 @@ const styles = StyleSheet.create({
     opacity: 1
   },
 
-  genericButton2: {
+  background: {
+    top: 0,
+    left: 0,
+    position: "absolute",
+    right: 0,
+    bottom: 0,
+    flexDirection: "column"
+  },
+  layoutStatusBar: {
+    height: Platform.OS === "android" ? 26.25 : 34,
+    alignSelf: "stretch"
+  },
+  rect: {
+    alignItems: "center",
+    backgroundColor: "#192734",
+    opacity: 1,
+    alignSelf: "stretch",
+    flex: 1,
+    justifyContent: "center",
+    flexGrow: undefined
+  },
+  logoR: {
     width: 265,
-    height: 44,
-    backgroundColor: "rgba(101,188,70,1)",
-    opacity: 1
+    height: 164,
+    marginTop: 0,
+    alignSelf: "center"
   },
-
-  genericButtonIcon: {
-    width: 265,
-    height: 41,
-    backgroundColor: "rgba(59,89,152,1)",
-    opacity: 1
+  containerButtons: {
+    width: 375,
+    height: 336,
+    alignItems: "center",
+    justifyContent: "space-between"
   },
-
-  genericButtonImage: {
+  text2: {
     width: 262,
-    height: 42,
-    backgroundColor: "rgba(129,129,129,1)",
-    opacity: 1
-  },
-
-  genericButton3: {
-    width: 263,
-    height: 44,
-    backgroundColor: "rgba(0,0,0,1)",
-    opacity: 1
-  },
-  others: {
-    width: 262,
-    height: 42
+    height: 17,
+    backgroundColor: "transparent",
+    textAlign: "center",
+    color: "#65bc46"
   },
   genericButton4: {
     width: 263,
@@ -173,13 +152,32 @@ const styles = StyleSheet.create({
     backgroundColor: "rgba(190,190,190,1)",
     opacity: 1
   },
-  rect: {
-    height: Platform.OS === "android" ? 25 : 32,
-    top: 0,
-    left: 0,
-    position: "absolute",
-    backgroundColor: "rgba(48,61,73,1)",
-    right: 0,
+  genericButton2: {
+    width: 265,
+    height: 44,
+    backgroundColor: "rgba(101,188,70,1)",
+    opacity: 1
+  },
+  others: {
+    width: 262,
+    height: 42
+  },
+  genericButtonIcon: {
+    width: 265,
+    height: 41,
+    backgroundColor: "rgba(59,89,152,1)",
+    opacity: 1
+  },
+  genericButtonImage: {
+    width: 262,
+    height: 42,
+    backgroundColor: "rgba(129,129,129,1)",
+    opacity: 1
+  },
+  genericButton3: {
+    width: 263,
+    height: 44,
+    backgroundColor: "rgba(0,0,0,1)",
     opacity: 1
   }
 });
